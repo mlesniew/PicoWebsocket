843d88a2637ffdd2498ca63bf2330d76cc7fa81f
Michał Leśniewski <mlesniew@gmail.com>
2026-04-29 21:15:23 +0200

    Add multi-client example

---
+++ examples/multi_client/src/multi_client.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <WiFiServer.h>
+
+#include <list>
+
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#error "This board is not supported."
+#endif
+
+#include <PicoWebsocket.h>
+
+#if __has_include("config.h")
+#include "config.h"
+#endif
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "WiFi SSID"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "password"
+#endif
+
+::WiFiServer server(80);
+PicoWebsocket::Server<::WiFiServer> websocket_server(server);
+
+std::list<PicoWebsocket::Server<::WiFiServer>::Client> websocket_clients;
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.println("Connecting to WiFi...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+    }
+    Serial.print("WiFi connected, IP: ");
+    Serial.println(WiFi.localIP());
+
+    websocket_server.begin();
+}
+
+void loop() {
+    auto websocket = websocket_server.accept();
+    if (websocket) {
+        Serial.println("New client connected");
+        websocket_clients.push_front(websocket);
+    }
+
+    for (auto it = websocket_clients.begin(); it != websocket_clients.end();) {
+        auto & websocket = *it;
+
+        if (!websocket.connected()) {
+            Serial.println("Client disconnected");
+            it = websocket_clients.erase(it);
+            continue;
+        }
+
+        if (websocket.available()) {
+            uint8_t buffer[128];
+            const auto bytes_read = websocket.read(buffer, 128);
+            Serial.printf("Received %u bytes\n", bytes_read);
+
+            // forward to all connected clients
+            for (auto & websocket2 : websocket_clients) {
+                websocket2.write(buffer, bytes_read);
+            }
+        }
+
+        ++it;
+    }
+}
